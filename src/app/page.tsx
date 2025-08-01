6053ebea6b14a89d66bb5ae8f45d94f7078f2ae3
ONEONUORA <monweebube44@gmail.com>
2025-08-01 12:02:43 +0100

    feat: add project set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
   return (
     <div>
-      <h1>Welcome to the FortiChain DApp</h1>
-      <p>This is the main page of the FortiChain decentralized application.</p>
+      <h1>Welcome to the Inheritx DApp</h1>
+      <p>This is the main page of the InheritX decentralized application.</p>
     </div>
   );
 }
