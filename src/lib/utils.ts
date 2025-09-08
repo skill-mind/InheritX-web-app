79813c028d771edeff31f28b80c01b09b7ad9d97
martinvibes <martinmachiebe21@gmail>
2025-09-08 17:03:27 +0100

    feat: add environment configuration and blockchain utilities

---
+++ src/lib/utils.ts
@@ -0,0 +1,32 @@
+import { clsx, type ClassValue } from "clsx";
+import { CairoCustomEnum, RpcProvider } from "starknet";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const myProvider = new RpcProvider({
+  nodeUrl: process.env.NEXT_PUBLIC_RPC_URL,
+});
+
+// For converting Hex to contract address
+export function bigintToHexAddress(value: bigint): `0x${string}` {
+  return `0x${value.toString(16)}`;
+}
+
+// For converting date to epoch time
+export function toEpochTime(date: string | Date): number {
+  const d = typeof date === "string" ? new Date(date) : date;
+  return Math.floor(d.getTime() / 1000);
+}
+
+// For creating a cairo enum
+export function createCairoEnum(value: string): CairoCustomEnum {
+  return new CairoCustomEnum({ [value]: {} });
+}
+
+// For copying text to clipboard
+export function copyToClipboard(text: string) {
+  navigator.clipboard.writeText(text);
+}
